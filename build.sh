e897037a6ff9d7d254461e1963f284d8530e6b6e
Philippe David <philippe.david0@gmail.com>
2018-04-10 00:30:36 -0400

    init bower

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ set -eo pipefail
 
 DEBUG=true
 DEPLOY=false
-# RELEASE=false
 TAG_NAME=""
 OUTPUT_STYLE="expanded"
-OUTPUT_NAME="funcss.css"
+OUTPUT_PATH="./dist"
+OUTPUT_NAME="funcss"
 
 function usage() {
     echo "\033[38;5;87mFUNCSS build Script\033[0m"
@@ -18,14 +18,13 @@ function usage() {
     echo "\033[0m./build.sh"
     echo "\033[38;5;13m\t-h --help"
     echo "\033[38;5;166m\t--debug [default=true]"
-    # echo "\t--release [default=false]"
     echo "\033[38;5;196m\t--deploy [default=false]"
     echo ""
 }
 
 # replace with the new tagged name in the index file.
 function replace_style_import() {
-    sed -i .bak "s/.*<link rel='stylesheet' href='.*'>.*/<link rel='stylesheet' href='${OUTPUT_NAME}'>/g" index.html
+    sed -i .bak "s/.*<link rel='stylesheet' href='.*'>.*/<link rel='stylesheet' href='$OUTPUT_NAME'>/g" index.html
     #not sure why; does not work without .bak syntax
     rm index.html.bak
 }
@@ -54,7 +53,7 @@ while [ "$1" != "" ]; do
             DEBUG=false
             TAG_NAME=$VALUE
             OUTPUT_STYLE="compressed"
-            OUTPUT_NAME="funcss-$TAG_NAME.css"
+            OUTPUT_NAME="funcss-$TAG_NAME"
             ;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
@@ -76,11 +75,31 @@ if [ "$DEBUG" = true ]; then
     open index.html
     # Open a new terminal and launch sass --watch
     # open -a terminal -e $PWD ""
-    sass --scss --watch css/_all.scss:$OUTPUT_NAME --style OUTPUT_STYLE
+    sass --scss --watch css/_all.scss:$OUTPUT_PATH/$OUTPUT_NAME.css --style $OUTPUT_STYLE
 else
-    sass --scss css/_all.scss:$OUTPUT_NAME --style OUTPUT_STYLE
+    sass --scss css/_all.scss:$OUTPUT_PATH/$OUTPUT_NAME.css --style $OUTPUT_STYLE
+
+    printf "\033[166;5m          minifying $OUTPUT_NAME           \033[0m\n"
+        
+    MIN_OUTPUT_NAME="$PWD$OUTPUT_PATH/$OUTPUT_NAME.min.css"
+        
+    curl -X POST -s --data-urlencode "input@$PWD$OUTPUT_PATH$OUTPUT_NAME.css" https://cssminifier.com/raw > $MIN_OUTPUT_NAME    
     # replace with the new tagged name in the index file.
+    OUTPUT_NAME=MIN_OUTPUT_NAME
     replace_style_import
+
+    if [ "$DEPLOY" = true ]; then
+        
+        git add dist/*
+        git add css/*
+        read commitmessage
+        git commit dist/* -m "$commitmessage"
+        git push
+        # printf "\033[106;30m        tag $TAG_NAME has been tagged        \033[0m\n"
+        # TODO: phil; find a way to auto inc. tag ?
+        # git tag TAG_NAME
+        # git push --all
+    fi
 fi
 
 printf "\033[106;30m                                          \033[0m\n"
@@ -92,12 +111,3 @@ printf "\033[106;30m ·▀▀▀▀  ▀▀▀  ▀▀▀▀  ▀ ·▀▀▀  �
 printf "\033[106;30m                                          \033[0m\n"
 printf "\033[106;30m      $OUTPUT_NAME has been compiled        \033[0m\n"
 
-if [ "$DEPLOY" = true ]; then
-    # minify
-    printf "\033[166;5m          minifying $OUTPUT_NAME           \033[0m\n"
-    curl -X POST -s --data-urlencode "input@$OUTPUT_NAME" https://cssminifier.com/raw > "$OUTPUT_NAME.min.css"
-    # TODO: phil; find a way to auto inc. tag ?
-    # git tag TAG_NAME
-    printf "\033[106;30m        tag ${TAG_NAME} has been tagged        \033[0m\n"
-    # git push --all
-fi
